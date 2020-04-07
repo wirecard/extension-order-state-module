bece2875ad061a8037467fca039944a2ad53b145
Flavius Aspra <flavius.aspra@wirecard.com>
2020-04-07 15:25:09 +0200

    Reformat test

--- tests/integration/FacadeTest.php
+++ tests/integration/FacadeTest.php
@@ -16,9 +16,26 @@ class FacadeTest extends PHPUnit_Framework_TestCase
     public function simple_cases_provider()
     {
         return [
-            [new PurchaseTransaction(), new TransactionType\None(), new Started(), new Pending(), "backend CC=purchase, engine=none, order state=started, desired state=pending"],
-            [new PurchaseTransaction(), new TransactionType\Success(), new Pending(), new Success(), "backend CC=purchase, engine=success, order state=started, desired state=success"],
-            [new PurchaseTransaction(), new TransactionType\Failure(), new Pending(), new Failed(), "backend CC=purchase, engine=success, order state=started, desired state=failed"],
+            [
+                new PurchaseTransaction(),//back-end is set to purchase
+                new TransactionType\None(),//there has been no interaction with the engine yet
+                new Started(),//the current state of the order is started
+                new Pending(),//the desired new state is pending
+                "backend CC=purchase, engine=none, order state=started, desired state=pending"
+            ],
+            [
+                new PurchaseTransaction(),
+                new TransactionType\Success(),
+                new Pending(), new Success(),
+                "backend CC=purchase, engine=success, order state=started, desired state=success"
+            ],
+            [
+                new PurchaseTransaction(),
+                new TransactionType\Failure(),//the engine said it's a failure
+                new Pending(),
+                new Failed(),//so we expect the order to go to failed
+                "backend CC=purchase, engine=success, order state=started, desired state=failed"
+            ],
         ];
     }
 
@@ -26,7 +43,12 @@ class FacadeTest extends PHPUnit_Framework_TestCase
      * @test Tests what should happen
      * @dataProvider simple_cases_provider
      */
-    public function all_simple_combinations(CreditCardTransactionType $creditCardTransactionType, TransactionType $engineTransactionType, State $currentOrderState, State $expected, $message)
+    public function all_simple_combinations(
+        CreditCardTransactionType $creditCardTransactionType,//backend setting for CC
+        TransactionType $engineTransactionType,//what the engine has said about this so far
+        State $currentOrderState,//the current state of the order
+        State $expected,//the desired state of the order after the transition
+        $message)//message, in case the assertion fails
     {
         $shopSystem = new DummyShopSystem($creditCardTransactionType);
         $order = new DummyOrder($currentOrderState, $engineTransactionType);
