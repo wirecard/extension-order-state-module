68e8f4dc54f6e4113e815b81853d224b69293a87
Flavius Aspra <flavius.aspra@wirecard.com>
2020-04-07 20:19:07 +0200

    Remove duplicate code

--- src/CreditCardTransactionType/PurchaseTransaction.php
+++ src/CreditCardTransactionType/PurchaseTransaction.php
@@ -9,13 +9,4 @@ use Wirecard\Order\State\Implementation\CreditCardTransactionTypeHelper;
 class PurchaseTransaction implements CreditCardTransactionType
 {
     use CreditCardTransactionTypeHelper;
-
-    /**
-     * @param CreditCardTransactionType $other
-     * @return bool
-     */
-    public function equals(CreditCardTransactionType $other)
-    {
-        return $this->strictlyEquals($other);
-    }
 }
