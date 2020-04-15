2531545e1e1b55be2f1dd0d362afec1e7c4cd622
ozada <oskar.huseynzada@wirecard.com>
2020-04-15 14:09:47 +0200

    Improve example. Fix codacy issue.

--- example/example.php
+++ example/example.php
@@ -1,118 +1,25 @@
 <?php
 
-namespace implementation;
+namespace example;
 
 ini_set("display_errors", true);
 
-$basePath = dirname(dirname(__FILE__));
-require_once $basePath . "/vendor/autoload.php";
+$pwd = dirname(__FILE__);
+require_once  dirname($pwd) . "/vendor/autoload.php";
+require_once $pwd . DIRECTORY_SEPARATOR . 'SampleInputTransferObject.php';
+require_once $pwd . DIRECTORY_SEPARATOR . 'SampleOrderStateMapper.php';
 
 
 use Wirecard\ExtensionOrderStateModule\Application\Service\OrderState;
 use Wirecard\ExtensionOrderStateModule\Domain\Entities\Constant;
-use Wirecard\ExtensionOrderStateModule\Domain\Entities\OrderState\Authorized;
-use Wirecard\ExtensionOrderStateModule\Domain\Entities\OrderState\Failed;
-use Wirecard\ExtensionOrderStateModule\Domain\Entities\OrderState\Pending;
-use Wirecard\ExtensionOrderStateModule\Domain\Entities\OrderState\Processing;
-use Wirecard\ExtensionOrderStateModule\Domain\Entities\OrderState\Started;
 use Wirecard\ExtensionOrderStateModule\Domain\Exception\InvalidValueException;
-use Wirecard\ExtensionOrderStateModule\Domain\Interfaces\InputDataTransferObject;
-use Wirecard\ExtensionOrderStateModule\Domain\Interfaces\OrderStateMapper;
 
-class DefaultMapper implements OrderStateMapper
-{
+$orderStateService = new OrderState(new SampleOrderStateMapper());
 
-    public function map()
-    {
-        return [
-            "started_external" => new Started(),
-            "pending_external" => new Pending(),
-            "failed_external" => new Failed(),
-            "authorized_external" => new Authorized(),
-            "processing_external" => new Processing(),
-        ];
-    }
-}
-
-$orderStateService = new OrderState(new DefaultMapper());
-
-class PrestashopDTO implements InputDataTransferObject
-{
-    private $processType;
-    private $transactionType;
-    private $transactionState;
-    private $currentOrderState;
-
-    /**
-     * @return mixed
-     */
-    public function getProcessType()
-    {
-        return $this->processType;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getTransactionType()
-    {
-        return $this->transactionType;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getTransactionState()
-    {
-        return $this->transactionState;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCurrentOrderState()
-    {
-        return $this->currentOrderState;
-    }
-
-    /**
-     * @param mixed $processType
-     */
-    public function setProcessType($processType)
-    {
-        $this->processType = $processType;
-    }
-
-    /**
-     * @param mixed $transactionType
-     */
-    public function setTransactionType($transactionType)
-    {
-        $this->transactionType = $transactionType;
-    }
-
-    /**
-     * @param mixed $transactionState
-     */
-    public function setTransactionState($transactionState)
-    {
-        $this->transactionState = $transactionState;
-    }
-
-    /**
-     * @param mixed $currentOrderState
-     */
-    public function setCurrentOrderState($currentOrderState)
-    {
-        $this->currentOrderState = $currentOrderState;
-    }
-
-
-}
 
 try {
     // Processing
-    $inputDTO = new PrestashopDTO();
+    $inputDTO = new SampleInputTransferObject();
     $inputDTO->setProcessType(Constant::PROCESS_TYPE_RETURN);
     $inputDTO->setTransactionType(Constant::TRANSACTION_TYPE_DEBIT);
     $inputDTO->setTransactionState(Constant::TRANSACTION_STATE_SUCCESS);
@@ -126,42 +33,6 @@ try {
     $inputDTO->setTransactionType(Constant::TRANSACTION_TYPE_DEBIT);
     $inputDTO->setTransactionState(Constant::TRANSACTION_STATE_FAILURE);
     $inputDTO->setCurrentOrderState(Constant::ORDER_STATE_STARTED);
-
-    $result = $orderStateService->process($inputDTO);
-    print_r($result . PHP_EOL);
-
-    // Pending
-    $inputDTO->setProcessType(Constant::PROCESS_TYPE_RETURN);
-    $inputDTO->setTransactionType(Constant::TRANSACTION_TYPE_PURCHASE);
-    $inputDTO->setTransactionState(Constant::TRANSACTION_STATE_SUCCESS);
-    $inputDTO->setCurrentOrderState(Constant::ORDER_STATE_STARTED);
-
-    $result = $orderStateService->process($inputDTO);
-    print_r($result . PHP_EOL);
-
-    // Pending
-    $inputDTO->setProcessType(Constant::PROCESS_TYPE_RETURN);
-    $inputDTO->setTransactionType(Constant::TRANSACTION_TYPE_PURCHASE);
-    $inputDTO->setTransactionState(Constant::TRANSACTION_STATE_SUCCESS);
-    $inputDTO->setCurrentOrderState(Constant::ORDER_STATE_PENDING);
-
-    $result = $orderStateService->process($inputDTO);
-    print_r($result . PHP_EOL);
-
-    // Pending
-    $inputDTO->setProcessType(Constant::PROCESS_TYPE_RETURN);
-    $inputDTO->setTransactionType(Constant::TRANSACTION_TYPE_AUTHORIZE);
-    $inputDTO->setTransactionState(Constant::TRANSACTION_STATE_SUCCESS);
-    $inputDTO->setCurrentOrderState(Constant::ORDER_STATE_PENDING);
-
-    $result = $orderStateService->process($inputDTO);
-    print_r($result . PHP_EOL);
-
-    // Failed
-    $inputDTO->setProcessType(Constant::PROCESS_TYPE_RETURN);
-    $inputDTO->setTransactionType(Constant::TRANSACTION_TYPE_AUTHORIZE);
-    $inputDTO->setTransactionState(Constant::TRANSACTION_STATE_SUCCESS);
-    $inputDTO->setCurrentOrderState(Constant::ORDER_STATE_FAILED);
 
     $result = $orderStateService->process($inputDTO);
     print_r($result . PHP_EOL);
